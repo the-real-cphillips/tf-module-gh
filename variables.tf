5515bd0a55605f1bd9d14df9cfa16784116caabc
Craig Phillips <cphillips@2u.com>
2021-03-10 21:34:39 -0500

    FMT to Clear Github Action

--- variables.tf
+++ variables.tf
@@ -5,50 +5,50 @@ variable "token" {
   sensitive   = true
 }
 variable "owner" {
-  type = string
+  type        = string
   description = "repo owner"
 }
 
 // Repo
 variable "default_branch_name" {
-  type = string
+  type        = string
   description = "Name of Default Branch"
-  default = "main"
+  default     = "main"
 }
 
 variable "repo_name" {
-  type = string
+  type        = string
   description = "Name of the repository"
 }
 
 variable "visibility" {
-  type = string
+  type        = string
   description = "Visibility of the Repo (private, internal, public)"
-  default = "private"
+  default     = "private"
 }
 
 // Repo - Booleans
 variable "auto_init" {
-  type = bool
+  type        = bool
   description = "Automatically init new repositories"
-  default = false
+  default     = false
 }
 
 variable "allow_merge_commit" {
-  type = bool
+  type        = bool
   description = "Allow or Disallow Merge Commits"
-  default = false
+  default     = false
 }
 
 variable "allow_squash_merge" {
-  type = bool
+  type        = bool
   description = "Allow or Disallow Squash Merge"
-  default = false
+  default     = false
 }
 
 variable "delete_branch_on_merge" {
-  type = bool
+  type        = bool
   description = "Whether to Delete Branches after Merge"
-  default = true
+  default     = true
 }
 
