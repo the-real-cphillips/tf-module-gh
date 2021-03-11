9ee4fc9fb66f1be9c6d4d8ba71785ca199371461
Craig Phillips <cphillips@2u.com>
2021-03-10 23:13:36 -0500

    Fixing Argument Ref

--- repo_file.tf
+++ repo_file.tf
@@ -1,6 +1,6 @@
 resource "github_repository_file" "terraform-fmt" {
   repository = github_repository.this.name
-  branch = github_branch_default.this.name
+  branch = github_branch_default.this.branch
   file = ".github/workflows/terraform.yml"
   content = "files/workflows/terraform.yml"
   commit_message = "Workflow Added by Terraform"
